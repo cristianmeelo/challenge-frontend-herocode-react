8696caa2ddc7c64c0e90984ece36cd143e2970db
cristianmeelo <cristianmeelo@gmail.com>
2023-12-01 23:03:39 -0300

    :sparkles: feat: update Welcome Component

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,23 +1,13 @@
 
-import { features } from "../../constants/welcome-text";
+
+
 import * as S from "./Welcome.styles";
 
 
-const Welcome = ({ isSidebarOpen }: DynamicContentProps) => {
+const Welcome = () => {
   return (
-    <S.Container isSidebarOpen={isSidebarOpen}>
-    <S.Title>Bem-vindo à Nossa Aplicação</S.Title>
-    <S.Description>
-      Explore os recursos e funcionalidades do nosso software. Veja como ele pode te ajudar:
-    </S.Description>
-    <S.FeaturesContainer>
-      {features.map((feature: Feature, index:number) => (
-        <S.Feature key={index}>
-          <S.FeatureIcon>{feature.icon}</S.FeatureIcon>
-          <S.FeatureText>{feature.text}</S.FeatureText>
-        </S.Feature>
-      ))}
-    </S.FeaturesContainer>
+    <S.Container >
+    <S.Title>Bem-vindo à Tech Challenge Hero Code</S.Title>
   </S.Container>
   );
 };
